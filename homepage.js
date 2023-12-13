fe6091c30586fd090eaf50dcfb08e3d90692e9c2
Liyema-dev <115063744+Liyema-dev@users.noreply.github.com>
2023-12-13 09:24:19 +0200

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,17 @@
+function loginUser(){
+    
+    email = document.getElementById('loginEmail').value;
+    password = document.getElementById('loginPassword').value;
+
+    fetch('http://127.0.0.1:8000/loginMember',{
+        method: 'POST',
+        body: {
+            'email':email,
+            'password':password
+        }
+    })
+    .then(response => {
+        console.log(response)
+    })
+
+}
