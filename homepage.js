097a0b77f975b2dd1087125d22d0342a3f153763
Liyema-dev <115063744+Liyema-dev@users.noreply.github.com>
2023-12-20 14:04:38 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -3,7 +3,7 @@ function loginUser(){
     email = document.getElementById('loginEmail').value;
     password = document.getElementById('loginPassword').value;
 
-    fetch('https://42d3-41-246-139-29.ngrok-free.app/loginMember',{
+    fetch('https://b7dc-41-246-129-247.ngrok-free.app/profile',{
         method: 'POST',
         body: {
             'email':email,
